77009702a7345fb7cf3ec6e89a3ef059ec4def27
Alex Klein <alex@facings.io>
2023-05-04 09:31:15 -0700

    update.

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ case 'PROJECTS':
   <a href="https://github.com/mewmix/Vyper-Experiments" onclick="window.open(this.href); return false;">Vyper Rock Paper Scissors Smart Contract &larr;</a><br>\
   <a href="https://github.com/mewmix/eth_block_bot_tg" onclick="window.open(this.href); return false;">Ethereum Time / Block Search &larr;</a><br>\
   <a href="https://github.com/mewmix/CryptoDashboard" onclick="window.open(this.href); return false;">Personal Dashboard for Linux/Unix systems &larr;</a><br>\
-  <a href="https://www.socalwebdev.com" onclick="window.open(this.href); return false;">Website &larr;</a><br>';
+  <a href="https://github.com/mewmix/socalwebdev.com" onclick="window.open(this.href); return false;">This Website &larr;</a><br>';
   break;
 
 case 'exit':
